e30ca9729193f0e2cd5ce0e917b13ee84f79901e
Jules <github@sirskunkalot.de>
2021-04-09 20:33:53 +0200

    cleanup dependencies

--- JotunnModStub/JotunnModStub.cs
+++ JotunnModStub/JotunnModStub.cs
@@ -5,30 +5,21 @@
 // Project: JotunnModStub
 
 using BepInEx;
-using HarmonyLib;
-using System.Reflection;
 using UnityEngine;
-using ValheimMod.UnityWrappers;
 
 namespace JotunnModStub
 {
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    [BepInDependency(JotunnLib.Main.ModGuid)]
     internal class JotunnModStubPlugin : BaseUnityPlugin
     {
         public const string PluginGUID = "com.jotunn.jotunnmodstub";
         public const string PluginName = "JotunnModStub";
         public const string PluginVersion = "0.0.1";
 
-        private Harmony m_harmony;
-        
         private void Awake()
         {
-            // Create harmony patches
-            m_harmony = new Harmony(PluginGUID);
-            m_harmony.PatchAll();
-
-            // Make sure the references for the Unity wrappers are loaded
-            Assembly.GetAssembly(typeof(ItemDropWrapper));
+            // Do all your init stuff here
         }
 
 #if DEBUG
@@ -39,11 +30,5 @@ namespace JotunnModStub
             }
         }
 #endif
-
-        private void OnDestroy()
-        {
-            // Remove harmony patches
-            m_harmony.UnpatchAll(PluginGUID);
-        }
     }
 }
