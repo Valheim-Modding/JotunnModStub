1f30964d78a792e29f9bdf0dd3c78b176a65a7a0
Paddy <paddy@bitnibbler.co.uk>
2021-04-12 19:08:27 +0100

    feat: issue with mockref fixing

--- JotunnModStub/JotunnModExample.cs
+++ JotunnModStub/JotunnModExample.cs
@@ -12,6 +12,10 @@ using System.Reflection;
 using JotunnLib.Managers;
 using Logger = JotunnLib.Logger;
 using JotunnLib.Configs;
+using System;
+using JotunnLib.Entities;
+using System.Collections.Generic;
+using System.IO;
 
 namespace JotunnModExample
 {
@@ -33,14 +37,25 @@ namespace JotunnModExample
         private Sprite testSprite;
         private GameObject backpackPrefab;
         private AssetBundle embeddedResourceBundle;
+        private bool clonedItemsAdded;
 
         private void Awake()
         {
             Config = base.Config;
             // Do all your init stuff here
             loadAssets();
-            addSkills();
+            addItemsWithConfigs();
+            addEmptyPiece();
+            addMockedItems();
+
+            On.ObjectDB.CopyOtherDB += addClonedItems;
+            ItemManager.OnAfterInit += () => 
+            {
+                //backpackPrefab.GetComponent<ParticleSystemRenderer>().gameObject;
+            };
+            
         }
+
 
         private void loadAssets()
         {
@@ -58,35 +73,166 @@ namespace JotunnModExample
 
             //Load embedded resources
             JotunnLib.Logger.LogInfo($"Embedded resources: {string.Join(",", Assembly.GetExecutingAssembly().GetManifestResourceNames())}");
-            embeddedResourceBundle = AssetUtils.LoadAssetBundleFromResources("capeironbackpack", Assembly.GetExecutingAssembly());
-            backpackPrefab = embeddedResourceBundle.LoadAsset<GameObject>("Assets/Evie/CapeIronBackpack.prefab");
+            embeddedResourceBundle = AssetUtils.LoadAssetBundleFromResources("eviesbackpacks1", Assembly.GetExecutingAssembly());
+            backpackPrefab = embeddedResourceBundle.LoadAsset<GameObject>("Assets/Evie/CapeSilverBackpack.prefab");
 
             // Embedded Resources
             
         }
 
-        void addSkills()
+        private void addClonedItems(On.ObjectDB.orig_CopyOtherDB orig, ObjectDB self, ObjectDB other)
         {
-            // Test adding a skill with a texture
-            Sprite testSkillSprite = Sprite.Create(testTex, new Rect(0f, 0f, testTex.width, testTex.height), Vector2.zero);
-            //TestSkillType = SkillManager.Instance.AddSkill("com.jotunnlib.JotunnModExample.testskill", "TestingSkill", "A nice testing skill!", 1f, testSkillSprite);
-            TestSkillType = SkillManager.Instance.AddSkill(new SkillConfig { Identifier = "com.jotunnlib.JotunnModExample.testskill",
-                Name = "TestingSkill",
-                Description = "A nice testing skill!",
-                Icon = testSkillSprite,
-                IncreaseStep = 1f
-            }, true);
-            Logger.LogDebug(TestSkillType);
-            //if(!TestSkillType) Logger.
+            // You want that to run only once, JotunnLib has the item cached for the game session
+            if (!clonedItemsAdded)
+            {
+                //Create a custom resource
+                CustomItem recipeComponent = new CustomItem("CustomWood", "Wood");
+                ItemManager.Instance.AddItem(recipeComponent);
+                recipeComponent.ItemDrop.m_itemData.m_shared.m_name = "$item_customWood";
+                recipeComponent.ItemDrop.m_itemData.m_shared.m_description = "$item_customWood_desc";
+
+                // Create and add a custom item based on SwordBlackmetal
+                CustomItem CI = new CustomItem("EvilSword", "SwordBlackmetal");
+                ItemManager.Instance.AddItem(CI);
+
+                // Replace vanilla properties of the custom item
+                var itemDrop = CI.ItemDrop;
+                itemDrop.m_itemData.m_shared.m_name = "$item_evilsword";
+                itemDrop.m_itemData.m_shared.m_description = "$item_evilsword_desc";
+
+                recipeEvilSword(itemDrop);
+
+                clonedItemsAdded = true;
+            }
+
+            // Hook is prefix, we just need to be able to get the vanilla prefabs, JotunnLib registers them in ObjectDB
+            orig(self, other);
         }
 
-       
+        private static void recipeEvilSword(ItemDrop itemDrop)
+        {
+            // Create and add a recipe for the copied item
+            Recipe recipe = ScriptableObject.CreateInstance<Recipe>();
+            recipe.name = "Recipe_EvilSword";
+            recipe.m_item = itemDrop;
+            recipe.m_craftingStation = PrefabManager.Cache.GetPrefab<CraftingStation>("piece_workbench");
+            recipe.m_resources = new Piece.Requirement[]
+            {
+                    new Piece.Requirement()
+                    {
+                        m_resItem = PrefabManager.Cache.GetPrefab<ItemDrop>("Stone"),
+                        m_amount = 1
+                    },
+                    new Piece.Requirement()
+                    {
+                        m_resItem = PrefabManager.Cache.GetPrefab<ItemDrop>("CustomWood"),
+                        m_amount = 1
+                    }
+            };
+            CustomRecipe CR = new CustomRecipe(recipe, false, false);
+            ItemManager.Instance.AddRecipe(CR);
+        }
+
+        // Add new Items with item Configs
+        private void addItemsWithConfigs()
+        {
+            // Add a custom piece table
+            PieceManager.Instance.AddPieceTable(BlueprintRuneBundle.LoadAsset<GameObject>("_BlueprintPieceTable"));
+            CreateBlueprintRune();
+            CreateRunePieces();
+
+            // Don't forget to unload the bundle to free the resources
+            BlueprintRuneBundle.Unload(false);
+        }
+
+        private void CreateBlueprintRune()
+        {
+            // Create and add a custom item
+            // CustomItem can be instantiated with an AssetBundle and will load the prefab from there
+            CustomItem rune = new CustomItem(BlueprintRuneBundle, "BlueprintRune", false);
+            ItemManager.Instance.AddItem(rune);
+
+            // Create and add a recipe for the custom item
+            CustomRecipe runeRecipe = new CustomRecipe(new RecipeConfig()
+            {
+                Item = "BlueprintRune",
+                Amount = 1,
+                Requirements = new PieceRequirementConfig[]
+                {
+                    new PieceRequirementConfig {Item = "Stone", Amount = 1}
+                }
+            });
+            ItemManager.Instance.AddRecipe(runeRecipe);
+        }
+
+        private void addEmptyPiece()
+        {
+            CustomPiece CP = new CustomPiece("$piece_lul", "Hammer");
+            var piece = CP.Piece;
+            piece.m_icon = testSprite;
+            var prefab = CP.PiecePrefab;
+            prefab.GetComponent<MeshRenderer>().material.mainTexture = testTex;
+            PieceManager.Instance.AddPiece(CP);
+        }
+
+        private void CreateRunePieces()
+        {
+            // Create and add custom pieces
+            GameObject makebp_prefab = BlueprintRuneBundle.LoadAsset<GameObject>("make_blueprint");
+            CustomPiece makebp = new CustomPiece(makebp_prefab, new PieceConfig
+            {
+                PieceTable = "_BlueprintPieceTable",
+                AllowedInDungeons = false
+            });
+            PieceManager.Instance.AddPiece(makebp);
+            GameObject placebp_prefab = BlueprintRuneBundle.LoadAsset<GameObject>("piece_blueprint");
+            CustomPiece placebp = new CustomPiece(placebp_prefab, new PieceConfig
+            {
+                PieceTable = "_BlueprintPieceTable",
+                AllowedInDungeons = true,
+                Requirements = new PieceRequirementConfig[]
+                {
+                    new PieceRequirementConfig {Item = "Wood", Amount = 2}
+                }
+            });
+            PieceManager.Instance.AddPiece(placebp);
+        }
+
+        private void addMockedItems()
+        {
+            if (!backpackPrefab) JotunnLib.Logger.LogWarning($"Failed to load asset from bundle: {embeddedResourceBundle}");
+            else
+            {
+                // Create and add a custom item
+                CustomItem CI = new CustomItem(backpackPrefab, true);
+                ItemManager.Instance.AddItem(CI);
+
+                //Create and add a custom recipe
+                Recipe recipe = ScriptableObject.CreateInstance<Recipe>();
+                recipe.m_item = backpackPrefab.GetComponent<ItemDrop>();
+                recipe.m_craftingStation = Mock<CraftingStation>.Create("piece_workbench");
+                var ingredients = new List<Piece.Requirement>
+                {
+                    MockRequirement.Create("LeatherScraps", 10),
+                    MockRequirement.Create("DeerHide", 2),
+                    MockRequirement.Create("Iron", 4),
+                };
+                recipe.m_resources = ingredients.ToArray();
+                CustomRecipe CR = new CustomRecipe(recipe, true, true);
+                ItemManager.Instance.AddRecipe(CR);
+
+                //Enable BoneReorder
+                BoneReorder.ApplyOnEquipmentChanged();
+            }
+            embeddedResourceBundle.Unload(false);
+        }
+
+
 #if DEBUG
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.F8))
             { // Set a breakpoint here to break on F6 key press
-                Player.m_localPlayer.RaiseSkill(TestSkillType, 1);
             }
         }
 #endif
