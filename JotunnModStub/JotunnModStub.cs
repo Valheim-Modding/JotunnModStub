7082bceb5cb7e785df731cb74b11ff8d3da4dbfe
Jules <github@sirskunkalot.de>
2021-04-29 22:14:45 +0200

    bump packages to match bepinex 5.4.10

--- JotunnModStub/JotunnModStub.cs
+++ JotunnModStub/JotunnModStub.cs
@@ -1,5 +1,5 @@
 ﻿// JotunnModStub
-// a Valheim mod skeleton using JötunnLib
+// a Valheim mod skeleton using Jötunn
 // 
 // File:    JotunnModStub.cs
 // Project: JotunnModStub
@@ -7,24 +7,27 @@
 using BepInEx;
 using UnityEngine;
 using BepInEx.Configuration;
+using Jotunn.Utils;
 
 namespace JotunnModStub
 {
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
     [BepInDependency(Jotunn.Main.ModGuid)]
-    //[NetworkCompatibilty(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
+    //[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
     internal class JotunnModStub : BaseUnityPlugin
     {
         public const string PluginGUID = "com.jotunn.jotunnmodstub";
         public const string PluginName = "JotunnModStub";
         public const string PluginVersion = "0.0.1";
-        public static new Jotunn.Logger Logger;
 
         private void Awake()
         {
             // Do all your init stuff here
             // Acceptable value ranges can be defined to allow configuration via a slider in the BepInEx ConfigurationManager: https://github.com/BepInEx/BepInEx.ConfigurationManager
             Config.Bind<int>("Main Section", "Example configuration integer", 1, new ConfigDescription("This is an example config, using a range limitation for ConfigurationManager", new AcceptableValueRange<int>(0, 100)));
+
+            // Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
+            Jotunn.Logger.LogInfo("ModStub has landed");
         }
 
 #if DEBUG
