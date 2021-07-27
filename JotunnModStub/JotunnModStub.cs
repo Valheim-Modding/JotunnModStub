92e083cadbdaf372b4c8bc038a5d25dd00894bb9
Jules <github@sirskunkalot.de>
2021-07-27 17:57:54 +0200

    use Jötunn v2.2.0

--- JotunnModStub/JotunnModStub.cs
+++ JotunnModStub/JotunnModStub.cs
@@ -5,9 +5,8 @@
 // Project: JotunnModStub
 
 using BepInEx;
-using UnityEngine;
 using BepInEx.Configuration;
-using Jotunn.Utils;
+using UnityEngine;
 
 namespace JotunnModStub
 {
@@ -23,8 +22,12 @@ namespace JotunnModStub
         private void Awake()
         {
             // Do all your init stuff here
-            // Acceptable value ranges can be defined to allow configuration via a slider in the BepInEx ConfigurationManager: https://github.com/BepInEx/BepInEx.ConfigurationManager
-            Config.Bind<int>("Main Section", "Example configuration integer", 1, new ConfigDescription("This is an example config, using a range limitation for ConfigurationManager", new AcceptableValueRange<int>(0, 100)));
+
+            // Acceptable value ranges can be defined to allow configuration via a slider in the BepInEx ConfigurationManager:
+            // https://github.com/BepInEx/BepInEx.ConfigurationManager
+            Config.Bind<int>("Main Section", "Example configuration integer", 1, 
+                new ConfigDescription("This is an example config, using a range limitation for ConfigurationManager", 
+                new AcceptableValueRange<int>(0, 100)));
 
             // Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
             Jotunn.Logger.LogInfo("ModStub has landed");
--- publish.ps1
+++ publish.ps1
@@ -27,17 +27,27 @@ Push-Location -Path (Split-Path -Parent $MyInvocation.MyCommand.Path)
     if (!(Test-Path "$_")) {Write-Error -ErrorAction Stop -Message "$_ folder is missing"}
 }
 
+# Plugin name without ".dll"
+$name = "$TargetAssembly" -Replace('.dll')
+
+# Create the mdb file
+$pdb = "$TargetPath\$name.pdb"
+if (Test-Path -Path "$pdb") {
+    Write-Host "Create mdb file for plugin $name"
+    Invoke-Expression "& `"$(Get-Location)\libraries\Debug\pdb2mdb.exe`" `"$TargetPath\$TargetAssembly`""
+}
+
 # Main Script
 Write-Host "Publishing for $Target from $TargetPath"
 
 if ($Target.Equals("Debug")) {
     Write-Host "Updating local installation in $ValheimPath"
     
-    $name = "$TargetAssembly" -Replace('.dll')
-
     $plug = New-Item -Type Directory -Path "$ValheimPath\BepInEx\plugins\$name" -Force
     Write-Host "Copy $TargetAssembly to $plug"
-    Copy-Item -Path "$TargetPath\$TargetAssembly" -Destination "$plug" -Force
+    Copy-Item -Path "$TargetPath\$name.dll" -Destination "$plug" -Force
+    Copy-Item -Path "$TargetPath\$name.pdb" -Destination "$plug" -Force
+    Copy-Item -Path "$TargetPath\$name.dll.mdb" -Destination "$plug" -Force
     
     $mono = "$ValheimPath\MonoBleedingEdge\EmbedRuntime";
     Write-Host "Copy mono-2.0-bdwgc.dll to $mono"
@@ -45,25 +55,18 @@ if ($Target.Equals("Debug")) {
         Copy-Item -Path "$mono\mono-2.0-bdwgc.dll" -Destination "$mono\mono-2.0-bdwgc.dll.orig" -Force
     }
     Copy-Item -Path "$(Get-Location)\libraries\Debug\mono-2.0-bdwgc.dll" -Destination "$mono" -Force
-
-    $pdb = "$TargetPath\$name.pdb"
-    if (Test-Path -Path "$pdb") {
-        Write-Host "Copy Debug files for plugin $name"
-        Copy-Item -Path "$pdb" -Destination "$plug" -Force
-        Start-Process -FilePath "$(Get-Location)\libraries\Debug\pdb2mdb.exe" -ArgumentList "`"$plug\$TargetAssembly`""
-    }
-
-    # Set dnspy debugger env - after a relog in Windows mono runtime listens on port 56000 instead of 55555
-    #$dnspy = '--debugger-agent=transport=dt_socket,server=y,address=127.0.0.1:56000,suspend=n,no-hide-debugger'
-    #[Environment]::SetEnvironmentVariable('DNSPY_UNITY_DBG2',$dnspy,'User')
+    
+    # set dnspy debugger env
+    #$dnspy = '--debugger-agent=transport=dt_socket,server=y,address=127.0.0.1:56000,suspend=y,no-hide-debugger'
+    #[Environment]::SetEnvironmentVariable('DNSPY_UNITY_DBG2','','User')
 }
 
 if($Target.Equals("Release")) {
     Write-Host "Packaging for ThunderStore..."
-    $PackagePath="$ProjectPath\Package"
+    $Package="Package"
+    $PackagePath=$ProjectPath+$Package
 
     Write-Host "$PackagePath\$TargetAssembly"
-    New-Item -Type Directory -Path "$PackagePath\plugins" -Force
     Copy-Item -Path "$TargetPath\$TargetAssembly" -Destination "$PackagePath\plugins\$TargetAssembly"
     Copy-Item -Path "$PackagePath\README.md" -Destination "$ProjectPath\README.md"
     Compress-Archive -Path "$PackagePath\*" -DestinationPath "$TargetPath\$TargetAssembly.zip" -Force
