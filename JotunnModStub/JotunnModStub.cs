5f05a8b45e7e4b2725c4162d6815fa7abed333c8
Jules <github@sirskunkalot.de>
2022-03-29 22:43:20 +0200

    use jötunn v2.6.1, remove mmhook ref

--- JotunnModStub/JotunnModStub.cs
+++ JotunnModStub/JotunnModStub.cs
@@ -25,27 +25,11 @@ namespace JotunnModStub
 
         private void Awake()
         {
-            // Jotunn comes with MonoMod Detours enabled for hooking Valheim's code
-            // https://github.com/MonoMod/MonoMod
-            On.FejdStartup.Awake += FejdStartup_Awake;
-            
             // Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
             Jotunn.Logger.LogInfo("ModStub has landed");
             
             // To learn more about Jotunn's features, go to
             // https://valheim-modding.github.io/Jotunn/tutorials/overview.html
-        }
-
-        private void FejdStartup_Awake(On.FejdStartup.orig_Awake orig, FejdStartup self)
-        {
-            // This code runs before Valheim's FejdStartup.Awake
-            Jotunn.Logger.LogInfo("FejdStartup is going to awake");
-
-            // Call this method so the original game method is invoked
-            orig(self);
-
-            // This code runs after Valheim's FejdStartup.Awake
-            Jotunn.Logger.LogInfo("FejdStartup has awoken");
         }
     }
 }
